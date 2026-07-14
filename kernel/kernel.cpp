786d7485cd2cd43a3239705de45b4f80a3d4cf28
xemoll <161621861+xemoll@users.noreply.github.com>
2026-07-14 19:17:49 +0100

    fix: remove hidden multilib libc dependency

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,5 +1,11 @@
-#include <stddef.h>
-#include <stdint.h>
+using uint8_t = unsigned char;
+using uint16_t = unsigned short;
+using uint32_t = unsigned int;
+using int32_t = int;
+using uintptr_t = unsigned int;
+using size_t = unsigned int;
+static_assert(sizeof(uint8_t) == 1 && sizeof(uint16_t) == 2 && sizeof(uint32_t) == 4);
+
 #include "generated/zenov_config.hpp"
 
 #include "parts/core.inc"
