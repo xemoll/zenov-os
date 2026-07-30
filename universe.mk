0996a6fb776ae0351df6f6a228d086ebca8287d7
xemoll <161621861+xemoll@users.noreply.github.com>
2026-07-30 18:15:37 +0500

    ZenPkg: build provider ABI v2 verification surfaces

--- universe.mk
+++ universe.mk
@@ -2,7 +2,7 @@
 ZENUNIVERSE_TOOL := $(BUILD)/zenuniverse
 ZENUNIVERSE_CATALOG := $(BUILD)/zenuniverse.zuc
 ZENUNIVERSE_SOURCES := $(sort $(wildcard packages/universe/*.zsource))
-ZENUNIVERSE_HEADERS := tools/zenuniverse/common.hpp tools/zenuniverse/descriptor.hpp tools/zenuniverse/host_profile.hpp tools/zenuniverse/resolver.hpp
+ZENUNIVERSE_HEADERS := tools/zenuniverse/common.hpp tools/zenuniverse/capability.hpp tools/zenuniverse/descriptor.hpp tools/zenuniverse/host_profile.hpp tools/zenuniverse/artifact_manifest.hpp tools/zenuniverse/resolver.hpp
 ZENUNIVERSE_CHECK_STAMP := $(BUILD)/zenuniverse-check.stamp
 ZENUNIVERSE_RUNTIME_CHECK_STAMP := $(BUILD)/zenuniverse-runtime-check.stamp
 
@@ -19,7 +19,7 @@ $(ZENUNIVERSE_CHECK_STAMP): $(ZENUNIVERSE_CATALOG) tests/zenuniverse_test.sh
 	@touch $@
 
 $(ZENUNIVERSE_RUNTIME_CHECK_STAMP): $(ZENUNIVERSE_CATALOG) tests/zenuniverse_runtime_provider_test.sh
-	BUILD=$(BUILD)/zenuniverse-runtime-test CXX=$(CXX) bash tests/zenuniverse_runtime_provider_test.sh
+	BUILD=$(BUILD)/zenuniverse-runtime-test CXX=$(CXX) CLANGXX=clang++ bash tests/zenuniverse_runtime_provider_test.sh
 	@touch $@
 
 .PHONY: universe-check
